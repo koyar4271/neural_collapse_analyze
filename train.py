d26ce36ed3070899a8741cb85aea93d10529343b
koyar4271 <koya.right.stuff@gmail.com>
2025-06-24 19:56:19 +0900

    add datasets & directory options

--- train.py
+++ train.py
@@ -1,6 +1,12 @@
+'''
+python train.py --dataset mnist --save_directory mnist_exp1
+Supported datasets: 'mnist', 'cifar10', 'cifar100'
+'''
+
 import torch
 import torch.nn as nn
 import torch.optim as optim
+import argparse
 
 from pathlib import Path
 from tqdm import tqdm
@@ -11,7 +17,18 @@ device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 if torch.cuda.is_available():
     print(f"Using GPU: {torch.cuda.get_device_name(0)}")
 
-DATASET = 'mnist'
+parser = argparse.ArgumentParser()
+parser.add_argument("--dataset", type=str, required=True,
+                    help="Dataset name: 'mnist', 'cifar10', or 'cifar100'")
+parser.add_argument("--save_directory", type=str, default=None,
+                    help="Optional subdirectory name under ./features/. Defaults to dataset name if not specified.")
+args = parser.parse_args()
+
+DATASET = args.dataset.lower()
+assert DATASET in ['mnist', 'cifar10', 'cifar100'], "Unsupported dataset"
+
+SAVE_DIR = args.save_directory if args.save_directory else DATASET
+
 config = get_config(DATASET)
 
 batch_size = config['batch_size']
@@ -21,14 +38,19 @@ weight_decay = config['weight_decay']
 momentum =config['momentum']
 lr_milestones = config['lr_milestones']
 
-train_loader, test_loader = get_dataloaders(dataset_name=DATASET, batch_size=batch_size)
+train_loader, test_loader, eval_loader = get_dataloaders(dataset_name=DATASET, batch_size=batch_size)
 
 if DATASET == 'mnist':
-    from models.MNIST_model import PapyanCNN
-    model = PapyanCNN().to(device)
+    # from models.MNIST_model import PapyanCNN
+    from models.MNIST_model_ResNet import ResNet18_MNIST
+    # model = PapyanCNN().to(device)
+    model = ResNet18_MNIST().to(device)
 elif DATASET == 'cifar10':
     from models.CIFAR10_model import PapyanResNet18
     model = PapyanResNet18().to(device)
+elif DATASET == 'cifar100':
+    from models.CIFAR100_model import PapyanResNet50
+    model = PapyanResNet50().to(device)
 else:
     raise ValueError("Invalid dataset")
 
@@ -38,8 +60,8 @@ optimizer = optim.SGD(model.parameters(), lr=lr, momentum=momentum, weight_decay
 scheduler = optim.lr_scheduler.MultiStepLR(optimizer, milestones=lr_milestones, gamma=0.1)
 # how to decrease lr, lr *= 0.1 at lr_milestones
 
-save_dir = Path('./features') # directory to save features
-save_dir.mkdir(exist_ok=True)
+save_dir = Path('./features') / SAVE_DIR # directory to save features
+save_dir.mkdir(parents=True, exist_ok=True)
 
 for epoch in range(1, num_epochs + 1):
     model.train() # switch to training mode
@@ -63,8 +85,8 @@ for epoch in range(1, num_epochs + 1):
 
     scheduler.step()
 
-    acc = correct / total * 100
-    print(f"→ Loss={total_loss/total:.4f}, Acc={acc:.2f}%")
+    train_acc = correct / total * 100
+    print(f"→ Loss={total_loss/total:.4f}, Acc={train_acc:.2f}%")
 
     model.eval()
     correct_test = 0
@@ -79,12 +101,11 @@ for epoch in range(1, num_epochs + 1):
     test_acc = correct_test / total_test * 100
     print(f"→ Test Acc={test_acc:.2f}%")
 
-    # 特徴とラベルを保存
     model.eval() # switch to evaluation mode
     all_feats = []
     all_labs = []
     with torch.no_grad():
-        for x, y in tqdm(train_loader, desc="Extracting", leave=False):
+        for x, y in tqdm(eval_loader, desc="Extracting", leave=False):
             x = x.to(device)
             feats = model.extract_features(x)
             all_feats.append(feats.cpu())
@@ -96,7 +117,7 @@ for epoch in range(1, num_epochs + 1):
                 'labels': labs,
                 'classifier_weight': model.classifier.weight.detach().cpu(),
                 'train_loss': total_loss / total,
-                'train_accuracy': acc,
+                'train_accuracy': train_acc,
                 'test_accuracy': test_acc
                 },
                 save_dir / f'epoch_{epoch:03d}.pt')
